6ed132ba470f392719ad38419e298115489dc919
GitHub Action <action@github.com>
2025-09-30 12:40:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/joshuataylor/intellijelixirhelper/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/joshuataylor/intellijelixirhelper/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.joshuataylor.intellijelixirhelper.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.joshuataylor.intellijelixirhelper.MyBundle
+import com.github.joshuataylor.intellijelixirhelper.services.MyProjectService
 import javax.swing.JButton
 
 
